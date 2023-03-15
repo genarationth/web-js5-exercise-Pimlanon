7e075f67a32ccfb82f33cf538e304ac7eac8c354
Pimlanon <125030381+Pimlanon@users.noreply.github.com>
2023-03-15 11:40:24 +0000

    completed part2

--- exercise1.js
+++ exercise1.js
@@ -2,4 +2,8 @@
 const waitingPeople = ['Sofia', 'David', 'Juan'];
 console.log(waitingPeople); //[ 'Sofia', 'David', 'Juan' ]
 
-
+//part2
+waitingPeople.push('Sara', 'Augustin');
+console.log(waitingPeople); //[ 'Sofia', 'David', 'Juan', 'Sara', 'Augustin' ]
+waitingPeople.shift();
+console.log(waitingPeople); //[ 'David', 'Juan', 'Sara', 'Augustin' ]
