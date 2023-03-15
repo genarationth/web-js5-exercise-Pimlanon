bfed1cb14b9527fa58d8289712c033e144491f29
Pimlanon <125030381+Pimlanon@users.noreply.github.com>
2023-03-15 11:44:06 +0000

    completed part3

--- exercise1.js
+++ exercise1.js
@@ -7,3 +7,9 @@ waitingPeople.push('Sara', 'Augustin');
 console.log(waitingPeople); //[ 'Sofia', 'David', 'Juan', 'Sara', 'Augustin' ]
 waitingPeople.shift();
 console.log(waitingPeople); //[ 'David', 'Juan', 'Sara', 'Augustin' ]
+
+//part3
+waitingPeople.splice(1,0,'Renata');
+console.log(waitingPeople); //[ 'David', 'Renata', 'Juan', 'Sara', 'Augustin' ]
+waitingPeople.push('Elena');
+console.log(waitingPeople);//[ 'David', 'Renata', 'Juan', 'Sara', 'Augustin', 'Elena' ]
