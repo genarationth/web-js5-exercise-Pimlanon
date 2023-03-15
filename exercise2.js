915299cfc089bc0ffa4ef8a387836f70d17e4ca5
Pimlanon <125030381+Pimlanon@users.noreply.github.com>
2023-03-15 14:47:17 +0000

    completed

---
+++ exercise2.js
@@ -0,0 +1,18 @@
+let star = '';
+for (let i = 0; i < 5; i++) {
+    for (j = 0; j < 1; j++) {
+        star = star+('*');
+        console.log(star);
+    }
+}
+console.log('-------------');
+
+//THIS IS NOT NESTED LOOP
+// let stars = '';
+// for (let i = 0; i < 5; i++) {
+//      stars = stars+('*');
+//      console.log(stars);
+// }
+// console.log('-------------');
+
+
